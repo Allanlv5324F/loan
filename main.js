5b0c262bba6b72a6e213d63e7728267e6e6485c7
Allanlv5324F <80536814+Allanlv5324F@users.noreply.github.com>
2021-07-11 21:12:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function sign()
+{
+    window.alert("You Have Already Created A Account Redirect To Your Account?")
+    window.location = "account.html";
+}
+
+function loan()
+{
+    window.location = "loan.html";
+}
+
+function baby()
+{
+    window.alert("Your Bank Says : You Took A Loan From V.I.P LOAN SERVICE");
+    
+}
